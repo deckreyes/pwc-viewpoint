fed2e83c17526290a72f915a2930e56c196cabaa
deck-adobe <deckreyes@adobe.com>
2023-10-12 15:17:36 -0700

    Code updates to sample stock ticker

--- blocks/stockticker/stockticker.js
+++ blocks/stockticker/stockticker.js
@@ -4,7 +4,18 @@ var searchSymbol = searchSymbols.replace(/<div>|<\/div>/g, '');
 
 var stockSymbol = searchSymbol.match(/\S+/g);
 
+if (stockSymbol[1] === undefined){
+
+      // Do not display anything.
+      document.getElementById('stock-price-information').textContent = 'Stock Price End Point currently unavailable :(';
+      document.getElementById('stock-price').textContent = '';
+      document.getElementById('high-price').textContent = '';
+      document.getElementById('low-price').textContent = '';
+      document.getElementById('trade-date').textContent = '';
+}
+else {
 // Make an HTTP request to the Alpha Vantage API
+// fetch ('http://127.0.0.1:5500/css/stock-price.json')
 fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='+stockSymbol[1]+'&apikey=YOUR_API_KEY')
   .then(response => response.json())
   .then(data => {
@@ -19,16 +30,31 @@ fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='+stockSym
 
     // Extract the low price
     const lowPrice = data['Global Quote']['04. low'];
-    
+
+    // Extract the trade date
+    const tradeDate = data['Global Quote']['07. latest trading day'];
+   
+    // Display title
+    document.getElementById('stock-price-information').textContent = 'Stock Price Information (dynamically generated): ' ;
+
     // Display the stock price
     document.getElementById('stock-price').textContent = stockTicker + ': ' + stockPrice;
 
     // Display the high price
     document.getElementById('high-price').textContent = 'High Price: ' + highPrice;
 
+     // Display the low price
+     document.getElementById('low-price').textContent = 'Low Price: ' + lowPrice;
+
+      // Display the last trade date
+    document.getElementById('trade-date').textContent = 'Trade Date: ' + tradeDate;
+
   })
   .catch(error => {
     console.log('Error fetching stock price:', error);
   });
+}
 
-  //alert (stockSymbol[1])
+//  fetch ('http://127.0.0.1:5500/css/stock-price.json')
+//  alert (stockSymbol[1])
+//  fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=ADBE&apikey=YOUR_API_KEY')
